3ce0e49df070cb28e7e45447f1f2b424c4f3559e
Tyler Giczkowski <tgiczkowski@apharmony.com>
2026-05-07 14:40:40 -0500

    added high performing model

--- dataprep.py
+++ dataprep.py
@@ -1,32 +1,32 @@
 import os
 import pandas as pd
 
-root = "Not_Fall"
+root = "SisFall_dataset/SisFall_dataset"
 
 # uncomment to convert all files to .csv
-for filename in os.listdir( root ):
-    if filename.endswith(".txt"):
-        old_path = os.path.join(root, filename)
-        new_path = os.path.join(root, filename.replace(".txt", ".csv"))
-        os.rename(old_path, new_path)
+for subject_folder in os.listdir( root ):
+    subject_path = os.path.join(root, subject_folder)
+    for filename in os.listdir( subject_path ):
+        if filename.endswith(".txt"):
+            old_path = os.path.join(subject_path, filename)
+            new_path = os.path.join(subject_path, filename.replace(".txt", ".csv"))
+            os.rename(old_path, new_path)
 
 # uncomment to remove colums 6, 7, 8
-for filename in os.listdir( root ):
-    if filename.endswith(".csv"):
-        df = pd.read_csv(root + "/" + filename, header=None)
-        df = df.drop(columns=[6, 7, 8])
-        df.to_csv(root + "/" + filename, index = False, header = False)
+for subject_folder in os.listdir( root ):
+    subject_path = os.path.join(root, subject_folder)
+    for filename in os.listdir( subject_path ):
+        if filename.endswith(".csv"):
+            df = pd.read_csv(subject_path + "/" + filename, header=None)
+            df = df.drop(columns=[6, 7, 8])
+            df.to_csv(subject_path + "/" + filename, index = False, header = False)
         
 #uncomment to add timestamp col then add headers
-for filename in os.listdir( root ):
-    if filename.endswith(".csv"):
-        df = pd.read_csv(root + "/" + filename, header=None)
-        df.insert(0, "timestamp", df.index * 5)
-        df.columns = ["timestamp", "acc_x", "acc_y", "acc_z", "gyro_x", "gyro_y", "gyro_z"]
-        df.to_csv(root + "/" + filename, index = False)
-            
-            
-            
-            
-
-
+for subject_folder in os.listdir( root ):
+    subject_path = os.path.join(root, subject_folder)
+    for filename in os.listdir( subject_path ):
+        if filename.endswith(".csv"):
+            df = pd.read_csv(subject_path + "/" + filename, header=None)
+            df.insert(0, "timestamp", df.index * 5)
+            df.columns = ["timestamp", "acc_x", "acc_y", "acc_z", "gyro_x", "gyro_y", "gyro_z"]
+            df.to_csv(subject_path + "/" + filename, index = False)
